67e3d8cb0748897f4c481a2331a3402219ecd453
andres <andres@ip-192-168-12-64.ec2.internal>
2018-03-15 14:44:59 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -45,13 +45,14 @@ class App extends Component {
       console.log(item.id);
       this.gapiRequest('https://www.googleapis.com/analytics/v3/management/accounts/' + item.id + '/webproperties')
         // .then((res) => console.log(res));
-        .then((res) => this.setState({
-          properties: res.data.items.map(item => ({
-            name: item.name,
-            id: item.id,
-            accountId: item.accountId
-          }))
-        })
+        .then((res) => console.log(res)
+        // this.setState({
+        //   properties: res.data.items.map(item => ({
+        //     name: item.name,
+        //     id: item.id,
+        //     accountId: item.accountId
+        //   }))
+        // })
         )
     })
   }
